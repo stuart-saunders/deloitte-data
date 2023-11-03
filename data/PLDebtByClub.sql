902f6e4b914614a66b107c5a68297b0e81cbfabe
Stuart Saunders <github@stuartsaunders.net>
2023-11-03 22:39:47 +0000

    WiP

---
+++ data/PLDebtByClub.sql
@@ -0,0 +1,16 @@
+-- PLRevenueByClub Table Population
+
+-- (Season, Club, NetCash, SoftLoans, OtherLoans, NetDebt, LeaguePosition)
+--INSERT INTO PLDebtByClub VALUES(2015, '', , -, , -, )
+
+-- 2014/15
+INSERT INTO PLDebtByClub VALUES(2015, 'Chelsea', 1, -1097, 0, -1096, 1)
+INSERT INTO PLDebtByClub VALUES(2015, 'Newcastle United', 48, -335, 0, -287, 15)
+INSERT INTO PLDebtByClub VALUES(2015, 'Manchester United', 151, 0, -406, -255, 4)
+INSERT INTO PLDebtByClub VALUES(2015, 'Queens Park Rangers', -20, -173, 0, -193, 20)
+INSERT INTO PLDebtByClub VALUES(2015, 'Liverpool', -45, -115, 0, -160, 6)
+INSERT INTO PLDebtByClub VALUES(2015, 'Sunderland', -81, -20, -38, -139, 16)
+INSERT INTO PLDebtByClub VALUES(2015, 'Hull City', 1, 0, -78, -77, 18)
+INSERT INTO PLDebtByClub VALUES(2015, 'West Ham United', -17, -1, -49, -67, 12)
+INSERT INTO PLDebtByClub VALUES(2015, 'Southampton', 1, 0, -47, -46, 7)
+INSERT INTO PLDebtByClub VALUES(2015, 'Stoke City', 26, -59, 0, -33, 9)
