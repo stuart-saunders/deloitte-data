4ef6e37c4152754bf7440de16be731f215d90e62
Stuart Saunders <github@stuartsaunders.net>
2023-11-03 22:38:43 +0000

    add tables

---
+++ tables/PLDebtByClub.sql
@@ -0,0 +1,11 @@
+CREATE TABLE [dbo].[PLDebtByClub] (
+    [Id]              INT IDENTITY (1, 1) NOT NULL,
+    [Season]          INT NOT NULL,
+    [Club]            VARCHAR(30) NOT NULL,
+    [NetCash]         INT NOT NULL,
+    [SoftLoans]       INT NOT NULL,
+    [OtherLoans]      INT NOT NULL,
+    [NetDebt]         INT NOT NULL,
+    [LeaguePosition]  INT NOT NULL
+    CONSTRAINT [PK_PLDebtByClub] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
