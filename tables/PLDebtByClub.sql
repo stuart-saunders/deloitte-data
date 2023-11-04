d2ea635dafc06c2a0280b72b03431a8ad6849684
Stuart Saunders <github@stuartsaunders.net>
2023-11-04 15:18:15 +0000

    add columns

--- tables/PLDebtByClub.sql
+++ tables/PLDebtByClub.sql
@@ -1,7 +1,11 @@
+-- SELECT * FROM PLDebtByClub
+-- DROP TABLE PLDebtByClub
+
 CREATE TABLE [dbo].[PLDebtByClub] (
     [Id]              INT IDENTITY (1, 1) NOT NULL,
     [Season]          INT NOT NULL,
     [Club]            VARCHAR(30) NOT NULL,
+    [DebtRank]        INT NOT NULL,
     [NetCash]         INT NOT NULL,
     [SoftLoans]       INT NOT NULL,
     [OtherLoans]      INT NOT NULL,
