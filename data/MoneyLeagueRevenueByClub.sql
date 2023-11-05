024c72ca38f3030e45f8adb6a258bbdce8fc71b7
Stuart Saunders <github@stuartsaunders.net>
2023-11-05 22:30:58 +0000

    add files

---
+++ data/MoneyLeagueRevenueByClub.sql
@@ -0,0 +1,69 @@
+--- **********************
+--- *** VALUES IN EURO ***
+--- **********************
+
+-- 2020/21
+-- Nation, Season, Club, RevenueRank, Matchday, Broadcast, Commercial, Total, 
+-- TransferIncome, TransferSpend, NetTransferBalance, WageCosts, LeaguePosition)
+INSERT INTO MoneyLeagueRevenueByClub 
+VALUES('England', 2021, 'Manchester City', 1, 0.8, 335.9, 308.2, 644.9, 106.4, 218.8, -112.4, 399.8, 1)
+
+INSERT INTO MoneyLeagueRevenueByClub 
+VALUES('Spain', 2021, 'Real Madrid', 2, 8.6, 310.5, 321.6, 640.7, 131.4, 44.7, 86.8, 403.6, 1)
+
+INSERT INTO MoneyLeagueRevenueByClub 
+VALUES('Germany', 2021, 'Bayern Munich', 3, 11.7, 254.5, 345.2, 611.4, 32.5, NULL, NULL, NULL, 1)
+
+INSERT INTO MoneyLeagueRevenueByClub 
+VALUES('Spain', 2021, 'Barcelona', 4, 15.9, 289.6, 276.6, 582.1, 66.8, 90.8, -24.0, 488.96, 2)
+
+INSERT INTO MoneyLeagueRevenueByClub 
+VALUES('England', 2021, 'Manchester United', 5, 8.0, 287.8, 262.2, 558.0, 24.5, 130.7, -106.2, 362.7, 2)
+
+INSERT INTO MoneyLeagueRevenueByClub 
+VALUES('France', 2021, 'Paris Saint-Germain', 6, 17.0, 201.8, 337.4, 556.2, NULL, NULL, NULL, NULL, 2)
+
+INSERT INTO MoneyLeagueRevenueByClub 
+VALUES('England', 2021, 'Liverpool', 7, 8.3, 303.7, 238.4, 550.4, NULL, NULL, NULL, 352.3, 3)
+
+INSERT INTO MoneyLeagueRevenueByClub 
+VALUES('England', 2021, 'Chelsea', 8, 8.6, 309.0, 175.5, 493.1, 35.4, 249.4, -214.0, 379.7, 4)
+
+INSERT INTO MoneyLeagueRevenueByClub 
+VALUES('Italy', 2021, 'Juventus', 9, 7.8, 237.0, 188.7, 433.5, 44.0, 137.3, -93.3, 320.8, 4)
+
+INSERT INTO MoneyLeagueRevenueByClub 
+VALUES('England', 2021, 'Tottenham', 10, 2.1, 233.3, 170.8, 406.2, 15.0, 118.0, -103.1, 231.5, 7)
+
+INSERT INTO MoneyLeagueRevenueByClub 
+VALUES('England', 2021, 'Arsenal', 11, 4.3, 208.2, 154.0, 366.5, 25.7, 129.6, -103.9, 274.9, 8)
+
+INSERT INTO MoneyLeagueRevenueByClub 
+VALUES('Germany', 2021, 'Borussia Dortmund', 12, 1.4, 186.7, 149.5, 337.6, 26.0, 92.3, -66.2, 219.4, 3)
+
+INSERT INTO MoneyLeagueRevenueByClub 
+VALUES('Germany', 2021, 'Borussia Dortmund', 12, 1.4, 186.7, 149.5, 337.6, 26.0, 92.3, -66.2, 219.4, 3)
+
+INSERT INTO MoneyLeagueRevenueByClub 
+VALUES('Spain', 2021, 'Athletico Madrid', 13, 4.2, 230.5, 98.1, 332.8, 77.6, 67.1, 10.5, 249.6, 3)
+
+INSERT INTO MoneyLeagueRevenueByClub 
+VALUES('Italy', 2021, 'Internazionale', 14, 2.2, 216.0, 112.7, 330.9, 28.3, 99.6, -71.3, 254.8, 1)
+
+INSERT INTO MoneyLeagueRevenueByClub 
+VALUES('England', 2021, 'Leicester', 15, 0.7, 208.4, 46.4, 255.5, 51.2, 72.7, -21.5, 217.2, 5)
+
+INSERT INTO MoneyLeagueRevenueByClub 
+VALUES('England', 2021, 'West Ham', 16, 0.6, 184.2, 36.7, 221.5, 52.4, 61.1, -8.8, 146.2, 6)
+
+INSERT INTO MoneyLeagueRevenueByClub 
+VALUES('England', 2021, 'Wolves', 17, 0.8, 191.3, 27.2, 219.2, 85.4, 98.1, -12.7, 157.8, 13)
+
+INSERT INTO MoneyLeagueRevenueByClub 
+VALUES('England', 2021, 'Everton', 18, 0.3, 165.3, 52.5, 218.1, 54.6, 130.0, -75.4, 206.1, 10)
+
+INSERT INTO MoneyLeagueRevenueByClub 
+VALUES('Italy', 2021, 'AC Milan', 19, 0.0, 147.2, 69.1, 216.3, 28.4, 81.0, -52.6, 168.7, 2)
+
+INSERT INTO MoneyLeagueRevenueByClub 
+VALUES('Russia', 2021, 'Zenit', 20, 7.3, 43.2, 161.5, 212.0, 7.1, NULL, NULL, NULL, 1)
---
+++ tables/MoneyLeagueRevenueByClub.sql
@@ -0,0 +1,17 @@
+CREATE TABLE [dbo].[MoneyLeagueRevenueByClub] (
+    [Id]                  INT IDENTITY (1, 1) NOT NULL,
+    [NationId]            INT NOT NULL,
+    [Season]              INT NOT NULL,
+    [Club]                VARCHAR(30) NOT NULL,
+    [RevenueRank]         INT NOT NULL,
+    [MatchdayRevenue]     DECIMAL(5, 1) NOT NULL,
+    [BroadcastRevenue]    DECIMAL(5, 1) NOT NULL,
+    [CommercialRevenue]   DECIMAL(5, 1) NOT NULL,
+    [TotalRevenue]        DECIMAL(5, 1) NOT NULL,
+    [TransferIncome]      DECIMAL(5, 1) NULL,
+    [TransferExpenditure] DECIMAL(5, 1) NULL,
+    [NetTransferBalance]  DECIMAL(5, 1) NULL,
+    [WageCosts]           DECIMAL(5, 1) NULL,
+    [LeaguePosition]      INT NOT NULL,
+    CONSTRAINT [PK_MoneyLeagueRevenueByClub] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
