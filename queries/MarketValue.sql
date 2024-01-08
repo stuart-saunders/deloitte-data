1db671f7f7868956c23959647ff44b8aa714f359
Stuart Saunders <github@stuartsaunders.net>
2024-01-08 23:20:23 +0000

    WiP

--- queries/MarketValue.sql
+++ queries/MarketValue.sql
@@ -1,11 +1,13 @@
 -- Values in €m
 
--- SELECT * FROM MarketValue
---WHERE Market = 'Big5Other'
+/*
+SELECT * FROM MarketValue
+WHERE Market = 'Big5Other'
+*/
 
--- *************************************
--- * Year-on-Year Increases
--- *************************************
+--*************************************
+--* Year-on-Year Increases
+--*************************************
 SELECT 
     t1.Market AS Market,
     t1.Season AS Current_Season,
@@ -16,13 +18,31 @@ SELECT
     FORMAT(ROUND(((t1.Value - t0.Value) / t0.Value), 2), 'N', 'en-gb') AS Percentage_Change
 FROM MarketValue t1
 INNER JOIN MarketValue t0 
-    -- ON t1.Season = t0.Season + 1 --Per Year
-    ON t1.Season = t0.Season + 7 -- Per Period
+    ON t1.Season = t0.Season + 1 --Per Year
+    -- ON t1.Season = t0.Season + 7 -- Per Period
     AND t1.Market = t0.Market
-WHERE t1.Season = '2022'
+WHERE t0.Season >= '2014'
+--WHERE t1.Season = '2022'
 --WHERE t1.Market = 'Big5Top'
 --OR t1.Market = 'Big5Other'
 -- WHERE t1.Market = 'Non5Top'
 -- WHERE t1.Market = 'Non5Other'
 -- WHERE t1.Market = 'Gov'
-ORDER BY Percentage_Change DESC
+ORDER BY Current_Season 
+
+
+/*
+European Market Dominated by the top leagues Big 5 Nations of England, Spain, Germany, Italy and France
+
+Between 2015 and 2022, the Market as a whole has increased by 35% (21.8 => 29.5)
+
+Biggest growth, in percentage terms has been the International game (Governing Bodies and National Associations), 
+but still dwarfed in finanical terms by the top leagues of the Big 5.
+
+Big 5 top leagues represent 58% of the market in 2022, up from 55% in 2015.
+
+Between 2015 and 2022, Big 5's top leagues have had by far the biggest growth, at 43%, with the others trailing in the teens.
+
+Big 5 top leagues are increasing market share, and growing at more than 3 times the rate of the lower leagues in those countries,
+and way in excess of twice the rate of the leagues outside of the biggest nations.
+*/
