6b9cee175708240d1f4fe7c70bbc1c3e3899dfd3
Stuart Saunders <github@stuartsaunders.net>
2024-02-17 14:00:48 +0000

    WiP

---
+++ results/3-PL Clubs/1-Combined/Coefficient of Variation.sql
@@ -0,0 +1,8 @@
+SELECT
+    t1.Season,
+    SUM(t1.TotalRevenue) AS 'Total Revenue',
+    STDEV(t1.TotalRevenue) AS 'Std Deviation',
+    AVG(t1.TotalRevenue) AS Mean,
+    (STDEV(t1.TotalRevenue) / AVG(t1.TotalRevenue)) * 100 AS 'Coefficient of Variation'
+FROM PLRevenueByClub t1
+GROUP BY t1.Season
